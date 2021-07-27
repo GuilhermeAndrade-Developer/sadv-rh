669b0dcf8a6992e38900b712cb80472865cc5dc5
Guilherme Andrade <guilhermeandrade.profissional@outlook.com>
2021-07-26 23:39:15 -0300

    feat: Criação Migration Employees

---
+++ database/migrations/2021_07_27_022526_create_employees_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('cpf');
+            $table->string('cep');
+            $table->string('address');
+            $table->integer('address_number');
+            $table->string('state');
+            $table->string('city');
+            $table->integer('telephone');
+            $table->unsignedBigInteger('companies_id');
+            $table->foreign('companies_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
